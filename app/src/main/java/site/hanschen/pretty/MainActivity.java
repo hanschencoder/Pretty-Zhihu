77088d7fbd644b87810c4082f47b75498ac0e3f3
shensky711 <shensky711@gmail.com>
2017-05-02 18:59:13 +0800

    initialization project

---
+++ app/src/main/java/site/hanschen/pretty/MainActivity.java
@@ -0,0 +1,27 @@
+package site.hanschen.pretty;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_toolbar)
+    Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(MainActivity.this);
+        initViews();
+    }
+
+    private void initViews() {
+        mToolbar.setTitle(R.string.app_name);
+        setSupportActionBar(mToolbar);
+    }
+}
